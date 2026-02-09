e3738eb17533621acb5f67eb413a73b93095a194
Karansinh2345 <dabhikaran1603@gmail.com>
2026-02-09 18:07:02 +0530

    Update for HuggingFace Secrets

--- config.py
+++ config.py
@@ -11,8 +11,7 @@ BRUSH_SHEET_NAME = "Brommer Brush Data"  # Target sheet for brush data
 # Dashboard Settings
 DASHBOARD_TITLE = "📊 Order Confirmation Live Dashboard"
 CURRENCY = "₹"
-<<<<<<< HEAD
+
 REFRESH_INTERVAL = 3600 # 60 minutes
-=======
-REFRESH_INTERVAL = 3600  # 5 minutes
->>>>>>> 44da3fed894a2823214b9359a2ee271ff70f96c9
+
+
