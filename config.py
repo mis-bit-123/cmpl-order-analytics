e3f358e3b493e54096b163373e8fc2d63c64e13a
mis-bit-123 <mis@coninfra.in>
2026-02-13 13:23:45 +0530

    add
    Updated the path for the credentials file.

--- config.py
+++ config.py
@@ -3,7 +3,7 @@
 # Google Sheets Configuration
 SHEET_ID = "169hKphPMANY80czkxcAZvqtvQMqfagC_jVnEuOZqhwk"  # Replace with your actual Sheet ID
 SHEET_NAME = "Order Confirmation"  # Your sub-sheet name
-CREDENTIALS_FILE = "needy-data/service_account.json"
+CREDENTIALS_FILE = "service_account.json"
 
 # Brush/Sweeper/Broomer Data Sheet
 BRUSH_SHEET_NAME = "Brommer Brush Data"  # Target sheet for brush data
